5571b6b67c21416c7e47748e75de5f19f0a983a7
claytonWeb3 <cody.clayton@web3levated.com>
2022-09-13 14:01:38 -0400

    staking contract skeleton

--- contracts/CoinchainStaking.sol
+++ contracts/CoinchainStaking.sol
@@ -2,17 +2,77 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CoinchainStaking {
+    /*/////////////////////////////////////////////////////////////
+                        DATA STRUCTURES 
+    /////////////////////////////////////////////////////////////*/
 
-  /*///////////////////////////////////////////////////////////////
-                    GLOBAL STATE
-    //////////////////////////////////////////////////////////////*/
+    struct Deposit {
+        address user;
+        uint256 amount;
+        uint256 lockUp;
+        uint256 depositTime;
+    }
 
+    /*/////////////////////////////////////////////////////////////
+                        GLOBAL STATE
+    /////////////////////////////////////////////////////////////*/
+
+    // Daily mintable allowance
+    uint256 dailyMintAllowance;
+    // Deposit ID incrementor
+    uint256 depositId;
     // CCH token
     IERC20 public CCH;
 
+    /*/////////////////////////////////////////////////////////////
+                        EVENTS
+    /////////////////////////////////////////////////////////////*/
+
+    event Deposit();
+    event withdrawl();
+    event Mint();
+
+    /*/////////////////////////////////////////////////////////////
+                        CONSTRUCTOR
+    /////////////////////////////////////////////////////////////*/
+
     constructor(address _CCHAddress) {
         CCH = IERC20(_CCHAddress);
-    }    
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function getDeposit(uint256 depositId) external view returns (Deposit) {
+
+    }
+
+    function getRewards(uint256 depositId) external view returns (uint256 rewards) {
+
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        PUBLIC FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function deposit(
+        address[] memory users,
+        uint256[] memory amounts,
+        uint256[] memory lockups,
+        uint256[] memory depositTimes
+    ) external onlyOwner {
+
+    }
+
+    function withdrawl(uint256 depositId) external onlyOwner {
+
+    }
+
+    function mint() external onlyOwner {
+
+    }
 }
