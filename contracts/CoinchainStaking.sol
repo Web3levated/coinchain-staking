9e169eaecfa42b9c3a2e265121c568718e1592c6
Louie <louis.miller@freaksnguilds.com>
2022-09-13 10:03:04 -0700

    create staking contract and test file

--- contracts/CoinchainStaking.sol
+++ contracts/CoinchainStaking.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
 contract CoinchainStaking {
-    
+
+  /*///////////////////////////////////////////////////////////////
+                    GLOBAL STATE
+    //////////////////////////////////////////////////////////////*/
+
+    // CCH token
+    IERC20 public CCH;
+
+    constructor(address _CCHAddress) {
+        CCH = IERC20(_CCHAddress);
+    }    
 }
---
+++ test/coinchain-staking.test.ts
@@ -0,0 +1,5 @@
+describe("CoinchainStaking", function () {
+    async function deployFixture(){
+        
+    }
+})
